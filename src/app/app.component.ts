3f365254bea96d18aa14868644fb2f3749195362
radeeb bashir <radeebbashir@gmail.com>
2017-06-12 01:29:26 -0500

    Deleted subscription codes and used async pipe to subscribe instead saving memory leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,12 @@ import { AngularFire} from 'angularfire2';
 export class AppComponent {
   title = 'app works!';
   cuisines;
-  private subscription;
 
   constructor(private af: AngularFire){
   }
 
   ngOnInit() {
-      this.subscription = this.af.database.list('/cuisines').subscribe(x => {
-          this.cuisines = x;
-          console.log(this.cuisines);
-      });
-  }
+      this.cuisines = this.af.database.list('/cuisines');
+}
 
-  ngOnDestroy() {
-      this.subscription.unsubscribe();
-  }
 }
