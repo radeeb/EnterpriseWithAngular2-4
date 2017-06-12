051cfc98cd17d61b0551987c7ddeac50e674d115
radeeb bashir <radeebbashir@gmail.com>
2017-06-12 02:41:56 -0500

    added database add/remove/update methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire} from 'angularfire2';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,36 @@ import { AngularFire} from 'angularfire2';
 })
 export class AppComponent {
   title = 'app works!';
-  cuisines;
+  cuisines: FirebaseListObservable<any[]>;
+  restaurant;
 
   constructor(private af: AngularFire){
   }
 
   ngOnInit() {
       this.cuisines = this.af.database.list('/cuisines');
+}
+
+add(){
+    this.cuisines.push({
+        name: 'Mexican',
+        details: {
+            description: '...'
+        }
+    });
+}
+
+update(){
+    this.af.database.object('/restaurant').update( {
+        name: 'New Name',
+        rating: 5
+    } );
+}
+
+remove(){
+    this.af.database.object('/restaurant').remove()
+        .then(x=> console.log("success"))
+        .catch(error => console.log("ERROR", error));
 }
 
 }
