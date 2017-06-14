759b91dc9225ed701a65ab8ef58f7ceae826613b
radeeb bashir <radeebbashir@gmail.com>
2017-06-14 02:03:42 -0500

    Modified to the smartweather app and implemented facebook login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,41 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  cuisines: FirebaseListObservable<any[]>;
-  restaurant;
+  displayName;
 
   constructor(private af: AngularFire){
   }
 
   ngOnInit() {
-      this.cuisines = this.af.database.list('/cuisines');
+      this.af.auth.subscribe(authState => {
+          if (!authState){
+            this.displayName = null;
+            console.log("Not logged in");
+            }
+          else{
+            console.log("Logged in", authState);
+            this.displayName = authState.auth.displayName;
+            }
+      });
 }
 
-add(){
-    this.cuisines.push({
-        name: 'Mexican',
-        details: {
-            description: '...'
-        }
+login() {
+    this.af.auth.login({
+        provider: AuthProviders.Facebook,
+        method: AuthMethods.Popup
+    }).then(authState => {
+        console.log("AFTER LOGIN", authState);
     });
 }
 
-update(){
-    this.af.database.object('/restaurant').update( {
-        name: 'New Name',
-        rating: 5
-    } );
+logout() {
+    this.af.auth.logout();
 }
-
-remove(){
-    this.af.database.object('/restaurant').remove()
-        .then(x=> console.log("success"))
-        .catch(error => console.log("ERROR", error));
-}
-
 }
